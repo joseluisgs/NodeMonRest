ea1cb18785ebdcf8fdda3a002dd59db40100f9a0
joseluisgs <joseluisgonsan@gmail.com>
2020-05-11 10:51:02 +0200

    tokenRefresh AirBnb Stilo JS

--- src/models/tokenRefresh.js
+++ src/models/tokenRefresh.js
@@ -4,14 +4,13 @@
  * https://mongoosejs.com/
  */
 
-'use strict';
 
 /**
  * Esquema de Usuarios
  */
-const db = require ('../database');
 const mongoose = require('mongoose');
 const conf = require('dotenv');
+const db = require('../database');
 
 // Cargamos la configuración del fichero .env
 const SETTINGS = conf.config();
@@ -19,50 +18,50 @@ const SETTINGS = conf.config();
 
 // Creación del esquema
 const TokenRefreshSchema = new mongoose.Schema(
-    {
-        // Le indicamos que expire
-        expireAt: {
-            type: Date,
-            default: Date.now,
-            index: { expires: (SETTINGS.parsed.TOKEN_REFRESH*1000*60) }  
-        },
-        username: { type: String, required: true },
-        uuid: { type: String, required: true }
+  {
+    // Le indicamos que expire
+    expireAt: {
+      type: Date,
+      default: Date.now,
+      index: { expires: (SETTINGS.parsed.TOKEN_REFRESH * 1000 * 60) },
     },
-    // El método estriccto nos dice si aceptamos o no un documento incpleto. Lo ponemos así porque no vamos a meter el id y da un poco de flexibilidad
-    {strict: false},
-    // Le añadimos una huella de tiempo
-    {timestamps: true }
+    username: { type: String, required: true },
+    uuid: { type: String, required: true },
+  },
+  // El método estriccto nos dice si aceptamos o no un documento incpleto. Lo ponemos así porque no vamos a meter el id y da un poco de flexibilidad
+  { strict: false },
+  // Le añadimos una huella de tiempo
+  { timestamps: true },
 );
 
 // Métodos estaticos que nos servirán para métodos rápidos
 
 // Devuelve el Iusuario por ID
-TokenRefreshSchema.statics.getById = function (id) {
-    return this.findOne({ _id: id })
-        .lean()                             // Con Lean le estamos diciendo que aprenda y la memorice porque la usaremos mucho
-        .exec();                            // Que lo ejecute
+TokenRefreshSchema.statics.getById = function getById(id) {
+  return this.findOne({ _id: id })
+    .lean() // Con Lean le estamos diciendo que aprenda y la memorice porque la usaremos mucho
+    .exec(); // Que lo ejecute
 };
 
 // Devuelve el Iusuario por Username
-TokenRefreshSchema.statics.getByUserName = function (username) {
-    return this.findOne({ username: username })
-        .lean()                             
-        .exec();                            
+TokenRefreshSchema.statics.getByUserName = function getByUserName(username) {
+  return this.findOne({ username })
+    .lean()
+    .exec();
 };
 
 // Devuleve el usuario por UUID
-TokenRefreshSchema.statics.getByUUID = function (uuid) {
-    return this.findOne({ uuid: uuid })
-        .lean()                            
-        .exec();                            
+TokenRefreshSchema.statics.getByUUID = function getByUUID(uuid) {
+  return this.findOne({ uuid })
+    .lean()
+    .exec();
 };
 
 // Sobre escribimos el método JSON, esto es porque si hacemos una vista necesitamos id y no _id que es como lo guarda Mongo
-TokenRefreshSchema.method('toJSON', function() {
-    const { __v, _id, ...object } = this.toObject();
-    object.id = _id;
-    return object;
+TokenRefreshSchema.method('toJSON', function toJSON() {
+  const { __v, _id, ...object } = this.toObject();
+  object.id = _id;
+  return object;
 });
 
 // Creamos un modelo del esquema
