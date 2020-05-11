9361d219eddec9e18b26b35814354be417603a73
joseluisgs <joseluisgonsan@gmail.com>
2020-05-11 10:19:08 +0200

    Ruta File AirBnb Stilo JS

--- src/routes/files.js
+++ src/routes/files.js
@@ -1,15 +1,14 @@
 /**
- * ENRUTADOR DE FICHEROS 
+ * ENRUTADOR DE FICHEROS
  * Enruta las peticiones para la subida de ficheros
  */
 
-'use strict';
 
 // Cargamos librerías, podemos usar la sitaxis EM6: import { Router } from 'express';
 const express = require('express');
 const filesController = require('../controllers/files');
-const auth = require('../middlewares/auth').auth;
-const permit = require('../middlewares/auth').permit;
+const { auth } = require('../middlewares/auth');
+const { permit } = require('../middlewares/auth');
 
 // Cargamos el enrutador
 const router = express.Router();
@@ -17,17 +16,17 @@ const router = express.Router();
 // Ruta POST File, sube un fichero, si estás identificado
 router.post('/upload', auth, filesController.addFiles);
 
-//ruta GET File, lista todos los ficheros. Solo admin
-router.get('/all', auth, permit(['admin']), filesController.files); 
+// ruta GET File, lista todos los ficheros. Solo admin
+router.get('/all', auth, permit(['admin']), filesController.files);
 
 // GET Obtiene un elemento por por ID
-router.get('/file/:id', auth, filesController.fileById); 
+router.get('/file/:id', auth, filesController.fileById);
 
 // GET Obtiene los ficheros del usuario actual
-router.get('/me', auth, filesController.myFiles); 
+router.get('/me', auth, filesController.myFiles);
 
 // DELETE Elimina el fichero
-router.post('/delete/:id', auth, filesController.deleteFileById); 
+router.post('/delete/:id', auth, filesController.deleteFileById);
 
 // De la misma manera podríamos hacer un CRUD completo, pero no es el caso
 
