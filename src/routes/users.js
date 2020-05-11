53f46778f2f6b4a45fc6cad966ccbfad87504226
joseluisgs <joseluisgonsan@gmail.com>
2020-05-11 10:13:03 +0200

    ruta User AriBbn JS Stilo

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,7 +3,6 @@
  * Enruta las peticiones al recurso de usuarios llamando al controlador y métodos adecuados
  */
 
-'use strict';
 
 // Cargamos librerías, podemos usar la sitaxis EM6: import { Router } from 'express';
 const express = require('express');
@@ -14,29 +13,30 @@ const usersController = require('../controllers/users');
 // Cargamos el enrutador
 const router = express.Router();
 
-// Para securizar ponemos auth en la ruta que queramos. Por ejemplo vamos a securizar añadir, modificar y borrar
+// Para securizar ponemos auth en la ruta que queramos. Debes estar identificados
 // Para más aclaraciones mirad el controlador recipes
-const auth = require('../middlewares/auth').auth; // es equivalente a poner const auth = requiere ('...').auth;
-const permit = require('../middlewares/auth').permit;
+const { auth } = require('../middlewares/auth'); // es equivalente a poner const auth = requiere ('...').auth;
+const { permit } = require('../middlewares/auth');
 
 
 // GET Listar todos los elementos, solo admin puede
-router.get('/', auth, permit(['admin']), usersController.users); 
+router.get('/', auth, permit(['admin']), usersController.users);
 
 // GET Obtiene un elemento por por ID
-router.get('/:id', auth, permit(['admin']), usersController.userById); 
+router.get('/:id', auth, permit(['admin']), usersController.userById);
 
 // POST Añadir Elemento. Solo el usuario administrador
 router.post('/', auth, permit(['admin']), usersController.addUser);
 
 // PUT Modifica un elemento por ID. Solo admin
-router.put('/:id', auth, permit(['admin']), usersController.editUserById); 
+router.put('/:id', auth, permit(['admin']), usersController.editUserById);
 
 // DELETE Elimina un elemento por ID. Solo Admin puede borrarlos.
-router.delete('/:id', auth, permit(['admin']), usersController.deleteUserById); 
+router.delete('/:id', auth, permit(['admin']), usersController.deleteUserById);
 
-// PATCH Inserta o actualiza la imagen del un usuario obtenida por un formulario. Antes la hemos tenido que subir con subir con files.upload. Se la pasamos en el body
-router.patch('/:username/avatar', auth, usersController.avatarToUser); 
+// PATCH Inserta o actualiza la imagen del un usuario obtenida por un formulario.
+// Antes la hemos tenido que subir con subir con files.upload. Se la pasamos en el body
+router.patch('/:username/avatar', auth, usersController.avatarToUser);
 
 // Exprotamos el módulo
 module.exports = router;
