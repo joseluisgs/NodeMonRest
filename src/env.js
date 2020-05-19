732159664f0b29b59fd66fe315a7289140b82215
joseluisgs <joseluisgonsan@gmail.com>
2020-05-19 10:36:25 +0200

    procesamiento env con valores pre establecidos

--- src/env.js
+++ src/env.js
@@ -8,27 +8,28 @@ const conf = require('dotenv');
 // Cogemos el objeto que necesitamos .env
 conf.config(); // Toda la configuración parseada del fichero .env
 
+// Es importante que pongamos unos valores por defecto por si no están en el .env o defnidos en el sistema
 module.exports = {
   NODE_ENV: process.env.NODE_ENV,
-  ENV: process.env.ENV,
-  DEBUG: process.env.DEBUG,
-  HOST: process.env.HOST,
-  PORT: process.env.PORT,
-  TIMEZONE: process.env.TIMEZONE,
-  TOKEN_SECRET: process.env.TOKEN_SECRET,
-  TOKEN_LIFE: process.env.TOKEN_LIFE,
-  TOKEN_REFRESH: process.env.TOKEN_REFRESH,
-  BC_SALT: process.env.BC_SALT,
-  DB_DEBUG: process.env.DB_DEBUG,
-  DB_POOOLSIZE: process.env.DB_POOLSIZE,
+  ENV: process.env.ENV || 'development',
+  DEBUG: process.env.DEBUG || true,
+  HOST: process.env.HOST || 'localhost',
+  PORT: process.env.PORT || 8080,
+  TIMEZONE: process.env.TIMEZONE || 'Europe/Madrid',
+  TOKEN_SECRET: process.env.TOKEN_SECRET || 'Este_Caballo_Viene_de_Boanzarrrrr_/_Lorem_Fistrum_Pecador_Te_Va_A_Haser_Pupitaa_Diodenaaalll_2020',
+  TOKEN_LIFE: process.env.TOKEN_LIFE || 20,
+  TOKEN_REFRESH: process.env.TOKEN_REFRESH || 40,
+  BC_SALT: process.env.BC_SALT || 10,
+  DB_DEBUG: process.env.DB_DEBUG || '', // puede ser true
+  DB_POOOLSIZE: process.env.DB_POOLSIZE || 200,
   DB_PROTOCOL: process.env.DB_PROTOCOL,
   DB_USER: process.env.DB_USER,
   DB_PASS: process.env.DB_PASS,
   DB_URL: process.env.DB_URL,
   DB_PORT: process.env.DB_PORT,
   DB_NAME: process.env.DB_NAME,
-  FILE_SIZE: process.env.FILE_SIZE,
-  FILES_PATH: process.env.FILES_PATH,
-  FILES_URL: process.env.FILES_URL,
+  FILE_SIZE: process.env.FILE_SIZE || 2,
+  FILES_PATH: process.env.FILES_PATH || 'files',
+  FILES_URL: process.env.FILES_URL || 'files',
   STORAGE: `${__dirname}/public/${process.env.FILES_PATH}/`,
 };
